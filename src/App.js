bd2911db1dbb53cfbbd362fb013552cbff7b2611
pioncz <pioncz@gmail.com>
2022-12-17 14:24:15 +0100

    fix path

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ const App = () => {
 
   useEffect(() => {
     faceapi
-      .loadTinyFaceDetectorModel('./models')
+      .loadTinyFaceDetectorModel('/classroom/models')
       .then((x) => {
         setFaceApiReady(true);
       })
